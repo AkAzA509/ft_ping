eca241bd11bf02f57b72ba3c4016674c4a911daa
vm_ecole <vm@ecole.com>
2026-08-17 13:21:12 +0200

    Add color to parsing log and help

--- parsing.c
+++ parsing.c
@@ -6,41 +6,40 @@
 #include <ctype.h>
 #include <string.h>
 
-void print_tab(char *tab[])
-{
-	if (!tab)
-		return ;
-	for (size_t i = 0; tab[i]; ++i)
-		printf("tab[%zu]: %s\n", i, tab[i]);
-}
+// void print_tab(char *tab[])
+// {
+// 	if (!tab)
+// 		return ;
+// 	for (size_t i = 0; tab[i]; ++i)
+// 		printf("tab[%zu]: %s\n", i, tab[i]);
+// }
 
 static void print_help(void)
 {
-	fprintf(stderr, "Usage\n  ft_ping [options] <destination>\n\n");
+	fprintf(stderr, BLD_WHITE"Usage\n  ft_ping [options] <destination>\n\n");
 	fprintf(stderr, "Options:\n");
 	fprintf(stderr, "  <destination>  DNS name or IP address\n");
 	fprintf(stderr, "  -v             verbose output\n");
 	fprintf(stderr, "  -? -h          display help list\n");
 	fprintf(stderr, "  -c <number>    stop after sending <number> packets\n");
-	fprintf(stderr, "  -ttl <number>  set <number> as the packet time-to-live\n");
+	fprintf(stderr, "  -ttl <number>  set <number> as the packet time-to-live\n"RESET);
 }
 
 static void parse_options(const char *opt, const char *next, bool *consumed)
 {
-	// while(*opt) {
 	if (*opt == 'v')
 		g_params.opts |= 1 << 1;
 	else if (*opt == 'c') {
 		g_params.opts |= 1 << 2;
 		if (!next || !isdigit((unsigned char)next[0]))
-			clean_exit("ft_ping: missing value after -c option\n", 2);
+			clean_exit(BLD_RED"ft_ping: missing value after -c option\n"RESET, 2);
 		g_params.c_val = atoi(next);
 		*consumed = true;
 	}
 	else if (!strcmp(opt, "ttl")) {
 		g_params.opts |= 1 << 3;
 		if (!next || !isdigit((unsigned char)next[0]))
-			clean_exit("ft_ping: missing value after -ttl option\n", 2);
+			clean_exit(BLD_RED"ft_ping: missing value after -ttl option\n"RESET, 2);
 		g_params.ttl_val = atoi(next);
 		*consumed = true;
 	}
@@ -48,8 +47,6 @@ static void parse_options(const char *opt, const char *next, bool *consumed)
 		print_help();
 		clean_exit("", 2);
 	}
-	// 	opt++;
-	// }
 }
 
 static void parse_address(char *addr)
@@ -60,7 +57,7 @@ static void parse_address(char *addr)
 		g_params.addr_cap = 2;
 		g_params.addr = malloc(sizeof(char *) * g_params.addr_cap);
 		if (!g_params.addr)
-			clean_exit("ft_ping: error malloc\n", 2);
+			clean_exit(BLD_RED"ft_ping: error malloc\n"RESET, 2);
 		memset(g_params.addr, 0, sizeof(char *) * g_params.addr_cap);
 	}
 
@@ -71,7 +68,7 @@ static void parse_address(char *addr)
 		size_t new_cap = g_params.addr_cap * 2;
 		void *tmp = realloc(g_params.addr, sizeof(char *) * new_cap);
 		if (!tmp)
-			clean_exit("ft_ping: error malloc\n", 2);
+			clean_exit(BLD_RED"ft_ping: error malloc\n"RESET, 2);
 
 		g_params.addr = tmp;
 		memset(g_params.addr + g_params.addr_cap, 0,
@@ -81,7 +78,7 @@ static void parse_address(char *addr)
 
 	g_params.addr[i] = strdup(addr);
 	if (!g_params.addr[i])
-		clean_exit("ft_ping: error malloc\n", 2);
+		clean_exit(BLD_RED"ft_ping: error malloc\n"RESET, 2);
 }
 
 void parse_params(char *av[])
@@ -96,7 +93,7 @@ void parse_params(char *av[])
 				av++;
 			}
 		}
-		else if (*av && isalpha(**av))
+		else if (*av /*&& isalpha(**av)*/)
 			parse_address(*av);
 		else {
 			print_help();
@@ -106,7 +103,7 @@ void parse_params(char *av[])
 	}
 
 	if (!g_params.addr)
-		clean_exit("ft_ping: error no destination provided\n", 2);
+		clean_exit(BLD_RED"ft_ping: error no destination provided\n"RESET, 2);
 
 	// printf("params:\n");
 	// printf("\toptions: %c %s %zu %s %zu\n", V_MASK(g_params.opts) ? 'v' : ' ',
